a0da6b846d1130737440d1af029d35801bc37902
Anubhav Pathak <pathak.anubhavdtu@gmail.com>
2025-07-28 11:02:32 +0530

    update js file

--- src/script.js
+++ src/script.js
@@ -1,3 +1,17 @@
+var _0x1234 = ['log', 'Hello']; 
+console[_0x1234[0]](_0x1234[1]);
+
+
+document.addEventListener("contextmenu", e => e.preventDefault());
+document.onkeydown = function(e) {
+    if (e.key === "F12" || (e.ctrlKey && e.shiftKey && e.key === "I")) {
+        return false;
+    }
+};
+
+
+
+
 const username = "anubhavpathak03";
 
 fetch("https://leetcode-stats-api.herokuapp.com/" + username)
@@ -24,9 +38,16 @@ const name = this.name.value.trim();
 const email = this.email.value.trim();
 const message = this.message.value.trim();
 
-const mailtoLink = `mailto:anubhavpathak783@gmail.com?subject=Message from ${encodeURIComponent(name)}&body=${encodeURIComponent(message)}%0A%0AEmail: ${encodeURIComponent(email)}`;
+if (!name || !email || !message) {
+    alert("Please fill all fields.");
+    return;
+}else {
+    const mailtoLink = `mailto:anubhavpathak783@gmail.com?subject=Message from ${encodeURIComponent(name)}&body=${encodeURIComponent(message)}%0A%0AEmail: ${encodeURIComponent(email)}`;
+    
+    window.location.href = mailtoLink;  //open default mail on sender side and pre fill the mail
+}
 
-window.location.href = mailtoLink;  //open default mail on sender side and pre fill the mail
+
 
 // alert("Hey Thanks🤗!. If you want to give any advice, good or bad, please DM me on my socials. Sorry for any inconvenience caused. Hope you understand ❤️.")
 
